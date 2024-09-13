5562286569f9a19465e1e63790964077efd49f0e
Den4ik1010 <den4ikpig@gmail.com>
2024-09-13 14:30:42 +0300

    Щоденик

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello")
+﻿using static System.Formats.Asn1.AsnWriter;
+
+Console.Write("Enter rating:");
+double rating1 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter rating:");
+double rating2 = double.Parse(Console.ReadLine());
+
+
+Console.Write("Enter rating:");
+double rating3 = double.Parse(Console.ReadLine());
+
+
+Console.Write("Enter rating:");
+double rating4 = double.Parse(Console.ReadLine());
+
+
+Console.Write("Enter rating:");
+double rating5 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Result Average score");
+Console.Write((rating1 + rating2 + rating3 + rating4 + rating5) /5);
+
+
