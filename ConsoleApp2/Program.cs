c6c30c09609b216f3cbcbf8f037cfeb0c73e6721
mka <mka@A34PC8>
2024-09-16 13:56:46 +0300

    Degrees in Fahrenheit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,24 +1,34 @@
-﻿using static System.Formats.Asn1.AsnWriter;
+﻿//using static System.Formats.Asn1.AsnWriter;
 
-Console.Write("Enter rating:");
-double rating1 = double.Parse(Console.ReadLine());
+//Console.Write("Enter rating:");
+//double rating1 = double.Parse(Console.ReadLine());
 
-Console.Write("Enter rating:");
-double rating2 = double.Parse(Console.ReadLine());
-
-
-Console.Write("Enter rating:");
-double rating3 = double.Parse(Console.ReadLine());
+//Console.Write("Enter rating:");
+//double rating2 = double.Parse(Console.ReadLine());
 
 
-Console.Write("Enter rating:");
-double rating4 = double.Parse(Console.ReadLine());
+//Console.Write("Enter rating:");
+//double rating3 = double.Parse(Console.ReadLine());
 
 
-Console.Write("Enter rating:");
-double rating5 = double.Parse(Console.ReadLine());
+//Console.Write("Enter rating:");
+//double rating4 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Result Average score");
-Console.Write((rating1 + rating2 + rating3 + rating4 + rating5) /5);
+
+//Console.Write("Enter rating:");
+//double rating5 = double.Parse(Console.ReadLine());
+
+//Console.WriteLine("Result Average score");
+//Console.Write((rating1 + rating2 + rating3 + rating4 + rating5) /5);
+
+
+Console.WriteLine("----------------Degrees in Fahrenheit----------------");
+Console.Write("Enter Degrees:");
+double degrees = double.Parse(Console.ReadLine());
+Console.Write("Result:");
+Math.Round(degrees);
+Console.Write(degrees * 1.800000 + 32);
+
+
 
 
