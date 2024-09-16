965862e993272cae41ffaf16639c5f2106b87425
mka <mka@A34PC8>
2024-09-16 14:25:04 +0300

    revolutions

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -22,12 +22,21 @@
 //Console.Write((rating1 + rating2 + rating3 + rating4 + rating5) /5);
 
 
-Console.WriteLine("----------------Degrees in Fahrenheit----------------");
-Console.Write("Enter Degrees:");
-double degrees = double.Parse(Console.ReadLine());
-Console.Write("Result:");
-Math.Round(degrees);
-Console.Write(degrees * 1.800000 + 32);
+//Console.WriteLine("----------------Degrees in Fahrenheit----------------");
+//Console.Write("Enter Degrees:");
+//double degrees = double.Parse(Console.ReadLine());
+//Console.Write("Result:");
+//Math.Round(degrees);
+//Console.Write(degrees * 1.800000 + 32);
+
+Console.WriteLine("-------------The revolution of Mars around the Sun in years-------------");
+Console.Write("Enter revolutions:");
+int day = int.Parse(Console.ReadLine());
+const int revolutions = 686;
+int day1 = day * revolutions;
+Console.WriteLine("Result:"+day1);
+
+
 
 
 
