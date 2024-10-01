83956ddf60ae5389f7cbc2e752f8e8cb4d40df48
den4ikpig@gmail.com <den4ikpig@gmail.com>
2024-10-01 20:29:19 +0300

    castom

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -18,53 +18,50 @@
 //Console.Write("Enter rating:");
 //double rating5 = double.Parse(Console.ReadLine());
 
-//Console.WriteLine("Result Average score");
-//Console.Write((rating1 + rating2 + rating3 + rating4 + rating5) /5);
+//Console.Write("Result Average score");
+//Console.Write((rating1 + rating2 + rating3 + rating4 + rating5) / 5);
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 //Console.WriteLine("----------------Degrees in Fahrenheit----------------");
 //Console.Write("Enter Degrees:");
 //double degrees = double.Parse(Console.ReadLine());
 //Console.Write("Result:");
 //Math.Round(degrees);
 //Console.Write(degrees * 1.800000 + 32);
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 //Console.WriteLine("-------------The revolution of Mars around the Sun in years-------------");
 //Console.Write("Enter revolutions:");
 //int day = int.Parse(Console.ReadLine());
 //const int revolutions = 686;
 //int day1 = day * revolutions;
-//Console.WriteLine("Result:"+day1);
+//Console.WriteLine("Result:" + day1);
 
-//using System.Linq.Expressions;
-//using System.Transactions;
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Console.Write("Enter your age:");
 //int age = int.Parse(Console.ReadLine());
-//if (age >= 15 && age <= 55) 
+//if (age >= 15 && age <= 55)
 //{
-//Console.ForegroundColor= ConsoleColor.Green;
+//    Console.ForegroundColor = ConsoleColor.Green;
 //    Console.WriteLine("So you can study");
-//    Console.ResetColor();   
+//    Console.ResetColor();
 
 //}
 //else
 //{
-//Console.ForegroundColor= ConsoleColor.Red;
+//    Console.ForegroundColor = ConsoleColor.Red;
 //    Console.WriteLine("SO you no can study:");
 //    Console.ResetColor();
 //}
 
 //Console.Write("Eneter you price:");
-//double price =double.Parse(Console.ReadLine());
+//double price = double.Parse(Console.ReadLine());
 
-//if (price >= 1000 && price <= 5000) 
+//if (price >= 1000 && price <= 5000)
 //{
-//    Console.ForegroundColor= ConsoleColor.Green;
+//    Console.ForegroundColor = ConsoleColor.Green;
 //    Console.WriteLine("Discounted price:10%");
 //    double price1 = price - price * (10 / 100.0);
 //    Console.Write($"Result: {price1} ");
-//   Console.ResetColor();
+//    Console.ResetColor();
 //}
 //if (price >= 5000)
 //{
@@ -76,10 +73,11 @@
 //}
 //else
 //{
-//    Console.ForegroundColor= ConsoleColor.Red;
+//    Console.ForegroundColor = ConsoleColor.Red;
 //    Console.WriteLine("No discount");
 //    Console.ResetColor();
 //}
+
 //Console.Write("Enter you rating:");
 //int rating = int.Parse(Console.ReadLine());
 
@@ -87,34 +85,34 @@
 //{
 //    Console.WriteLine("badly");
 //}
-//if(rating == 2)
+//if (rating == 2)
 //{
-// Console.WriteLine("satisfactorily");
+//    Console.WriteLine("satisfactorily");
 //}
-//if (rating ==3)
+//if (rating == 3)
 //{
-// Console.WriteLine("satisfactorily");
+//    Console.WriteLine("satisfactorily");
 //}
 //if (rating == 4)
 //{
-// Console.WriteLine("good");
+//    Console.WriteLine("good");
 //}
 //if (rating == 5)
 //{
-// Console.WriteLine("good");
+//    Console.WriteLine("good");
 //}
-Console.Write("choose currency (1-3):");
-int currency = int.Parse(Console.ReadLine());
+//Console.Write("choose currency (1-3):");
+//int currency =int.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter GRN:");
-int GRN = int.Parse(Console.ReadLine());
+//Console.Write("Enter GRN:");
+//int GRN = int.Parse(Console.ReadLine());
 
-switch (currency)
-{
-    case 1: Console.WriteLine($"Result: {GRN / 41.14}"); break;
-    case 2: Console.WriteLine($"Result: {GRN / 48}"); break;
-    case 3: Console.WriteLine($"Result: {GRN / 10.74}"); break;
-      default: Console.WriteLine("no"); break;
-}
+//switch (currency)
+//{
+//    case 1: Console.WriteLine($"Result: {GRN / 41.14}"); break;
+//    case 2: Console.WriteLine($"Result: {GRN / 48}"); break;
+//    case 3: Console.WriteLine($"Result: {GRN / 10.74}"); break;
+//    default: Console.WriteLine("no"); break;
+//}
 
 
