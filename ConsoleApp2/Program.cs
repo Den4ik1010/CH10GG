9c40d1fc8dd08f84328907f54b29692125ea95bb
den4ikpig@gmail.com <den4ikpig@gmail.com>
2024-10-11 20:17:31 +0300

    Random

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -122,31 +122,129 @@
 //    Console.Write("#");
 //}
 
-Console.Write("Enter width: ");
-int width = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Enter width: ");
+//int width = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter height: ");
-int height = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Enter height: ");
+//int height = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter symbols:");
-char symbols = char.Parse(Console.ReadLine());
+//Console.Write("Enter symbols:");
+//char symbols = char.Parse(Console.ReadLine());
 
-Console.Write("choose a collar from 1 to 3: 1-ed 2-green 3-blue");
-int color =int.Parse(Console.ReadLine());
+//Console.Write("choose a collar from 1 to 3: 1-ed 2-green 3-blue");
+//int color =int.Parse(Console.ReadLine());
 
-switch(color)
+//switch(color)
+//{
+//    case 1: Console.ForegroundColor= ConsoleColor.Red; break;
+
+//    case 2: Console.ForegroundColor = ConsoleColor.Blue; break;
+
+//    case 3: Console.ForegroundColor = ConsoleColor.Yellow; break;
+//}
+
+
+//for (int i = 0; i < height; i++)
+//{
+
+//    Console.WriteLine(new string(symbols, width));
+//}
+var rand = new Random();
+int winsInRow = 0;
+
+Console.Write("Put your money $: ");
+double money = int.Parse(Console.ReadLine());
+
+while (money > 0)
 {
-    case 1: Console.ForegroundColor= ConsoleColor.Red; break;
+    Console.Write("Guess the number [1-6]...");
+    int number = int.Parse(Console.ReadKey().KeyChar.ToString());
 
-    case 2: Console.ForegroundColor = ConsoleColor.Blue; break;
+    Console.Clear();
 
-    case 3: Console.ForegroundColor = ConsoleColor.Yellow; break;
+    int dice = rand.Next(1, 7);
+
+    DrawDice(dice);
+
+    if (number == dice)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("You won! +50%");
+        money = Math.Round(money * 1.5, 2, MidpointRounding.ToZero);
+        ++winsInRow;
+
+        if (number == 3)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Happy number! +30%");
+            money = Math.Round(money * 1.3, 2, MidpointRounding.ToZero);
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("You loose! -20%");
+        money = Math.Round(money / 1.2, 2, MidpointRounding.ToZero);
+        winsInRow = 0;
+    }
+    if (winsInRow >= 2)
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Jackpot! +100%");
+        money = Math.Round(money * 2, 2, MidpointRounding.ToZero);
+    }
+
+    Console.ResetColor();
+
+    Console.WriteLine($"Balance: {money}$");
 }
 
-
-for (int i = 0; i < height; i++)
+static void DrawDice(int roll)
 {
-
-    Console.WriteLine(new string(symbols, width));
+    switch (roll)
+    {
+        case 1:
+            Console.WriteLine("+-------+");
+            Console.WriteLine("|       |");
+            Console.WriteLine("|   o   |");
+            Console.WriteLine("|       |");
+            Console.WriteLine("+-------+");
+            break;
+        case 2:
+            Console.WriteLine("+-------+");
+            Console.WriteLine("| o     |");
+            Console.WriteLine("|       |");
+            Console.WriteLine("|     o |");
+            Console.WriteLine("+-------+");
+            break;
+        case 3:
+            Console.WriteLine("+-------+");
+            Console.WriteLine("| o     |");
+            Console.WriteLine("|   o   |");
+            Console.WriteLine("|     o |");
+            Console.WriteLine("+-------+");
+            break;
+        case 4:
+            Console.WriteLine("+-------+");
+            Console.WriteLine("| o   o |");
+            Console.WriteLine("|       |");
+            Console.WriteLine("| o   o |");
+            Console.WriteLine("+-------+");
+            break;
+        case 5:
+            Console.WriteLine("+-------+");
+            Console.WriteLine("| o   o |");
+            Console.WriteLine("|   o   |");
+            Console.WriteLine("| o   o |");
+            Console.WriteLine("+-------+");
+            break;
+        case 6:
+            Console.WriteLine("+-------+");
+            Console.WriteLine("| o   o |");
+            Console.WriteLine("| o   o |");
+            Console.WriteLine("| o   o |");
+            Console.WriteLine("+-------+");
+            break;
+    }
 }
 
