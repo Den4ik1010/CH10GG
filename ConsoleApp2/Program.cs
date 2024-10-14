9464f638f6e5fca77ab2e03f77920d77f8be4438
mka <mka@A34PC8>
2024-10-14 14:32:36 +0300

    Pythaski1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -149,102 +149,167 @@
 
 //    Console.WriteLine(new string(symbols, width));
 //}
-var rand = new Random();
-int winsInRow = 0;
+//var rand = new Random();
+//int winsInRow = 0;
 
-Console.Write("Put your money $: ");
-double money = int.Parse(Console.ReadLine());
+//Console.Write("Put your money $: ");
+//double money = int.Parse(Console.ReadLine());
 
-while (money > 0)
+//while (money > 0)
+//{
+//    Console.Write("Guess the number [1-6]...");
+//    int number = int.Parse(Console.ReadKey().KeyChar.ToString());
+
+//    Console.Clear();
+
+//    int dice = rand.Next(1, 7);
+
+//    DrawDice(dice);
+
+//    if (number == dice)
+//    {
+//        Console.ForegroundColor = ConsoleColor.Green;
+//        Console.WriteLine("You won! +50%");
+//        money = Math.Round(money * 1.5, 2, MidpointRounding.ToZero);
+//        ++winsInRow;
+
+//        if (number == 3)
+//        {
+//            Console.ForegroundColor = ConsoleColor.Blue;
+//            Console.WriteLine("Happy number! +30%");
+//            money = Math.Round(money * 1.3, 2, MidpointRounding.ToZero);
+//        }
+//    }
+//    else
+//    {
+//        Console.ForegroundColor = ConsoleColor.Red;
+//        Console.WriteLine("You loose! -20%");
+//        money = Math.Round(money / 1.2, 2, MidpointRounding.ToZero);
+//        winsInRow = 0;
+//    }
+//    if (winsInRow >= 2)
+//    {
+//        Console.ForegroundColor = ConsoleColor.Magenta;
+//        Console.WriteLine("Jackpot! +100%");
+//        money = Math.Round(money * 2, 2, MidpointRounding.ToZero);
+//    }
+
+//    Console.ResetColor();
+
+//    Console.WriteLine($"Balance: {money}$");
+//}
+
+//static void DrawDice(int roll)
+//{
+//    switch (roll)
+//    {
+//        case 1:
+//            Console.WriteLine("+-------+");
+//            Console.WriteLine("|       |");
+//            Console.WriteLine("|   o   |");
+//            Console.WriteLine("|       |");
+//            Console.WriteLine("+-------+");
+//            break;
+//        case 2:
+//            Console.WriteLine("+-------+");
+//            Console.WriteLine("| o     |");
+//            Console.WriteLine("|       |");
+//            Console.WriteLine("|     o |");
+//            Console.WriteLine("+-------+");
+//            break;
+//        case 3:
+//            Console.WriteLine("+-------+");
+//            Console.WriteLine("| o     |");
+//            Console.WriteLine("|   o   |");
+//            Console.WriteLine("|     o |");
+//            Console.WriteLine("+-------+");
+//            break;
+//        case 4:
+//            Console.WriteLine("+-------+");
+//            Console.WriteLine("| o   o |");
+//            Console.WriteLine("|       |");
+//            Console.WriteLine("| o   o |");
+//            Console.WriteLine("+-------+");
+//            break;
+//        case 5:
+//            Console.WriteLine("+-------+");
+//            Console.WriteLine("| o   o |");
+//            Console.WriteLine("|   o   |");
+//            Console.WriteLine("| o   o |");
+//            Console.WriteLine("+-------+");
+//            break;
+//        case 6:
+//            Console.WriteLine("+-------+");
+//            Console.WriteLine("| o   o |");
+//            Console.WriteLine("| o   o |");
+//            Console.WriteLine("| o   o |");
+//            Console.WriteLine("+-------+");
+//            break;
+//    }
+//}
+const int size = 4;
+int emptyX = 2;
+int emptyY = 2;
+
+int[,] puzzle = new int[size, size]
 {
-    Console.Write("Guess the number [1-6]...");
-    int number = int.Parse(Console.ReadKey().KeyChar.ToString());
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 0, 12},
+    {13, 14, 15, 11}
+};
+ShowPuzzle();
+
+while (true)
+{
+    var key = Console.ReadKey().Key;
+
+    switch (key)
+    {
+        case ConsoleKey.LeftArrow:
+            // left
+            Swap(ref puzzle[emptyY, emptyX], ref puzzle[emptyY, emptyX + 1]);
+            ++emptyX;
+            break;
+        case ConsoleKey.RightArrow:
+            // right
+            Swap(ref puzzle[emptyY, emptyX], ref puzzle[emptyY, emptyX - 1]);
+            --emptyX;
+            break;
+        case ConsoleKey.UpArrow:
+            // 
+            Swap(ref puzzle[emptyY, emptyX], ref puzzle[emptyY + 1, emptyX ]);
+            ++emptyY;
+            break;
+        case ConsoleKey.DownArrow:
+            // 
+            Swap(ref puzzle[emptyY, emptyX], ref puzzle[emptyY - 1 , emptyX ]);
+            --emptyY;
+            break;
+    }
 
     Console.Clear();
-
-    int dice = rand.Next(1, 7);
-
-    DrawDice(dice);
-
-    if (number == dice)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("You won! +50%");
-        money = Math.Round(money * 1.5, 2, MidpointRounding.ToZero);
-        ++winsInRow;
-
-        if (number == 3)
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Happy number! +30%");
-            money = Math.Round(money * 1.3, 2, MidpointRounding.ToZero);
-        }
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("You loose! -20%");
-        money = Math.Round(money / 1.2, 2, MidpointRounding.ToZero);
-        winsInRow = 0;
-    }
-    if (winsInRow >= 2)
-    {
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("Jackpot! +100%");
-        money = Math.Round(money * 2, 2, MidpointRounding.ToZero);
-    }
-
-    Console.ResetColor();
-
-    Console.WriteLine($"Balance: {money}$");
+    ShowPuzzle();
 }
 
-static void DrawDice(int roll)
+void ShowPuzzle()
 {
-    switch (roll)
+    for (int r = 0; r < size; r++)
     {
-        case 1:
-            Console.WriteLine("+-------+");
-            Console.WriteLine("|       |");
-            Console.WriteLine("|   o   |");
-            Console.WriteLine("|       |");
-            Console.WriteLine("+-------+");
-            break;
-        case 2:
-            Console.WriteLine("+-------+");
-            Console.WriteLine("| o     |");
-            Console.WriteLine("|       |");
-            Console.WriteLine("|     o |");
-            Console.WriteLine("+-------+");
-            break;
-        case 3:
-            Console.WriteLine("+-------+");
-            Console.WriteLine("| o     |");
-            Console.WriteLine("|   o   |");
-            Console.WriteLine("|     o |");
-            Console.WriteLine("+-------+");
-            break;
-        case 4:
-            Console.WriteLine("+-------+");
-            Console.WriteLine("| o   o |");
-            Console.WriteLine("|       |");
-            Console.WriteLine("| o   o |");
-            Console.WriteLine("+-------+");
-            break;
-        case 5:
-            Console.WriteLine("+-------+");
-            Console.WriteLine("| o   o |");
-            Console.WriteLine("|   o   |");
-            Console.WriteLine("| o   o |");
-            Console.WriteLine("+-------+");
-            break;
-        case 6:
-            Console.WriteLine("+-------+");
-            Console.WriteLine("| o   o |");
-            Console.WriteLine("| o   o |");
-            Console.WriteLine("| o   o |");
-            Console.WriteLine("+-------+");
-            break;
+        for (int c = 0; c < size; c++)
+        {
+            if (puzzle[r, c] == 0)
+                Console.Write("[  ] ");
+            else
+                Console.Write($"[{puzzle[r, c],2}] ");
+        }
+        Console.WriteLine();
     }
 }
 
+static void Swap(ref int first, ref int second)
+{
+    int temp = first;
+    first = second;
+    second = temp;
+}
